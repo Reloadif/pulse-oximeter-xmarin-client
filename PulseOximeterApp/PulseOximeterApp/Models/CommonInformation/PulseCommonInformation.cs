6e5bcc9a9d9122f3e8db3204d8d7fa6f9fe84360
Yura Chornyi <fastistdast@list.ru>
2023-04-22 21:02:58 +0300

    Hotfix

--- PulseOximeterApp/PulseOximeterApp/Models/CommonInformation/PulseCommonInformation.cs
+++ PulseOximeterApp/PulseOximeterApp/Models/CommonInformation/PulseCommonInformation.cs
@@ -22,12 +22,34 @@ namespace PulseOximeterApp.Models
 
         public PulseCommonInformation(IList<int> values)
         {
-            int normalPercent = Convert.ToInt32(values.Where(v => v >= 45 && v <= 80).Count() / values.Count * 100);
+            int normalPulseMeasurement = Convert.ToInt32(values.Where(v => v >= 45 && v <= 80).Count() / values.Count * 100);
 
             AverageBPM = Convert.ToInt32(values.Average());
-            NormalPulseMeasurement = normalPercent;
-            Status = StatisticStatus.Good;
-            Recommendation = "";
+            NormalPulseMeasurement = normalPulseMeasurement;
+            Status = CalculateStatisticStatus(normalPulseMeasurement);
+            Recommendation = CalculateRecommendetion(normalPulseMeasurement);
+        }
+
+        private StatisticStatus CalculateStatisticStatus(int normalPulseMeasurement)
+        {
+            StatisticStatus result = StatisticStatus.Default;
+
+            if (normalPulseMeasurement < 80) result = StatisticStatus.VeryBad;
+            else if (normalPulseMeasurement < 90) result = StatisticStatus.Bad;
+            else if (normalPulseMeasurement <= 100) result = StatisticStatus.Good;
+
+            return result;
+        }
+
+        private string CalculateRecommendetion(int normalPulseMeasurement)
+        {
+            string result = "";
+
+            if (normalPulseMeasurement < 80) result = "Состояние оценивается, как очень плохое!";
+            else if (normalPulseMeasurement < 90) result = "Состояние оценивается, как плохое!";
+            else if (normalPulseMeasurement <= 100) result = "Состояние оценивается, как хорошое!";
+
+            return result;
         }
     }
 }
